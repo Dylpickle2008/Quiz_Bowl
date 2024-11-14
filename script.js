7b33cd2ef0c824dfc1ca45b2710a8f7bac1aa2a4
Dylpickle2008 <dylanrigby1016@gmail.com>
2024-11-14 13:32:34 +0000

    changed javascript again

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-const button = docuement.querySelector('.questions');
-const input = document.querySelector('.#input-field');
-const score = document.querySelector('.score');
+let questionList = ['How old is Joe Biden?', 'What is 2 + 2?', 'Where do we live?'];
+let answerList = ['81', '4', 'NY State'];
+let score = 0;
+let questionIndex = 0;
 
-const questionSwitch = document.getElementById('question-switch');
+const question = document.querySelector('.question-container')
+const button = docuement.querySelector('.button-container');
+const input = document.querySelector('#input-field');
+let scoreContainer = document.querySelector('.score-container');
 
-submitSwitch.addEventListener("click", () => {
-    document.getElementById('name').value = '';
+question.textContent = questionList[questionIndex];
 
-    currentQuestionIndex = (currentQuestionIndex + 1) % questionsArray.length; 
-    document.querySelector('.questions').textContent = questionsArray[currentQuestionIndex];
-});
+let submitFunction = () =>{
+    button.addEventListener('click', () =>{
+        if(input.value === answerList[questionIndex]){
+            questionIndex += 1;
+            score += 1;
+            question.textContent = questionList[questionIndex];
+            scoreContainer.textContent = "Score: " + score; 
+        } else{
+            score -= 1;
+            scoreContainer.textContent = "Score: " + score; 
+        }
+        //Put Endgame Function Here
+        endGame(); 
+    });
+}
 
-const questionsArray = [
-    "What is your name?",
-    "What is your age?",
-];
+let endGame = () =>{
+    if(questionIndex > 2){
+        scoreContainer.textContent = "Final Score: " + score; 
+        question.textContent = "Congratulations: Play Again!";
+        input.style.display = "none";
+        button.style.display = "none";
+    }
+}
 
-let currentQuestionIndex = 0;
+submitFunction();
